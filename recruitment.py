22a97374b149a4b17e8406fcc5cffb07d40586c4
Ahmed Hajeyah <hajeyah@gmail.com>
2022-07-07 18:12:14 +0300

    Solved in a different way

--- recruitment.py
+++ recruitment.py
@@ -10,6 +10,14 @@ def show_skills(skills):
     print("Available skills:")
     for i, element in enumerate(get_skills()):
         print("{}) {}".format(i + 1, element))
+
+"""
+print()
+print("Skills:")
+for index, skill in enumerate(skills, start=1):
+print(f" {index}. {skill}")
+print()
+"""
     
 
 
@@ -42,23 +50,21 @@ def get_user_cv(skills):
     # Get the users CV from their inputs
     # Write your code here
 #    cv = ({"name":"","age":"","yearexp":"","skills":""})
-    cv=({"name":input("What's your name? "),"age":input("How old are you? "),"yearexp":input("How many years of experience do you have? "),"skills":get_user_skills(skills)})
+    return ({"name":input("What's your name? "),"age":int(input("How old are you? ")),"yearexp":int(input("How many years of experience do you have? ")),"skills":get_user_skills(skills)})
 #    get_user_skills(skills)
-    print(cv)
-    return cv
+#    print(cv)
+#    return cv
 #get_user_cv(get_skills())
 
 
 def check_acceptance(cv, desired_skill):
     # Check if the cv is acceptable or not and return a boolean based on that
     # Write your code here
-    desired_skill=[get_skills()]
-    cv=get_user_cv(get_skills())
-    if ("40" <= cv["age"] >= "18") and cv["yearexp"] >= "3" and desired_skill in cv["skills"]:
-        return True
+    return (cv["age"]) >= 18 and (cv["age"]) <= 40 and cv["yearexp"] > 3 and desired_skill in cv["skills"]
+#        return True
 #        print("Congratulations, you have been accepted!")
-    else:
-        return False
+#    else:
+#        return False
 #    print("Sorry, you have not been accepted.")
 
 
@@ -66,13 +72,14 @@ def check_acceptance(cv, desired_skill):
 def main():
     # Write your main logic here by combining the functions above into the
     # desired outcome
-    print("Welcome to the special recruitment program, please answer the following questions:")
 #    skills=get_skills()
-#    get_user_cv(skills)
-    check_acceptance()
-
-"""
+    print("Welcome to the special recruitment program, please answer the following questions:")
+    cv = get_user_cv(get_skills())
+    if check_acceptance(cv, 'Python'):
+#    if decision:
+        print("Congratulations, you have been accepted! "+cv["name"])
+    else:
+        print("Sorry, you have not been accepted.")
 
 if __name__ == "__main__":
     main()
-    """
